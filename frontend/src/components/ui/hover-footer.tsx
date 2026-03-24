9c5f117195c0317b5b126c14f7a87f6e7e56abac
Ishaan-Rai09 <ishaanrai18@gmail.com>
2026-03-24 12:05:06 +0530

    Fix SVG viewBox width to prevent PROOFDROP text truncation

--- frontend/src/components/ui/hover-footer.tsx
+++ frontend/src/components/ui/hover-footer.tsx
@@ -36,7 +36,7 @@ export const TextHoverEffect = ({
       ref={svgRef}
       width="100%"
       height="100%"
-      viewBox="0 0 320 120"
+      viewBox="0 0 450 120"
       xmlns="http://www.w3.org/2000/svg"
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
